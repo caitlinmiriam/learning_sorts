5b8e8a9f0c9064c8eff07816629d3582ad3a7844
caitlinmiriam <caitlinmiriam@gmail.com>
2026-07-20 15:38:02 +0530

    Fix loop initialization in bubbleSort method

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,8 +2,10 @@
 class Solution {
     public void bubbleSort(int[] arr) {
         int n=arr.length;
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n-1;i++){ 
+            // Checking j with j+1
             for(int j=0;j<n-i-1;j++){
+                // At the end of this loop the largest element is moved to the end. So we do not need to disturb that element
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
